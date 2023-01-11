268559ca658c262fb81d4d2fd3810367b281c9d2
jdiaz <jorge.diazma@sansano.usm.cl>
2023-01-11 19:33:11 -0300

    docker react - django

--- APIBackend/APIBackend/settings.py
+++ APIBackend/APIBackend/settings.py
@@ -12,6 +12,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'applications.data',
 ]
 
@@ -23,7 +24,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+
 
 ROOT_URLCONF = 'APIBackend.urls'
 
